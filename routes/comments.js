4facbd832bbe7da385cc6c8314a5eb227aa608ac
Ryan Moore <rhcmoore@gmail.com>
2019-03-22 11:03:07 -0700

    Edit and delete comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var router = express.Router({mergeParams: true}); // gives access to :id param from app.js route prepend
+var router = express.Router({ mergeParams: true }); // gives access to :id param from app.js route prepend
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
 
@@ -30,8 +30,8 @@ router.post("/", isLoggedIn, function (req, res) {
                     comment.author.id = req.user._id;
                     comment.author.username = req.user.username;
                     comment.save();
-                    campground.comments.push(comment); 
-                    campground.save(); 
+                    campground.comments.push(comment);
+                    campground.save();
                     res.redirect('/campgrounds/' + campground._id);
                 }
             });
@@ -39,9 +39,44 @@ router.post("/", isLoggedIn, function (req, res) {
     });
 });
 
+// display edit comment form
+router.get("/:comment_id/edit", function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            // we have access to campground id with req.params.id
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+        }
+    });
+});
+
+// handle comment update
+router.put("/:comment_id", function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+});
+
+// delete comment
+router.delete("/:comment_id", function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err, commentRemoved) {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        };
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
